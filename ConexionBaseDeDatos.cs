0b2216bcc7b08c5ac5ac567427eaa2438cd0bef0
alosslessdev <ppaanncchhoo507@gmail.com>
2024-10-10 16:01:15 -0500

    poner metodos

--- ConexionBaseDeDatos.cs
+++ ConexionBaseDeDatos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +13,92 @@ namespace Lab_6
 
         private string detallesConexion = "Data Source=localhost;Initial Catalog=Libreria;Integrated Security=True";
 
+        public DataSet ObtenerDatos()
+        {
+            DataSet datos = new DataSet();
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(detallesConexion))
+                {
+                    string query = "SELECT Nombre, Departamento, Salario FROM Empleados";
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
+                    adaptador.Fill(datos, "Libros");
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error al obtener libros: " + ex.Message);
+            }
+
+            return datos;
+
+        }
+
+        public DataSet InsertDatos()
+        {
+            DataSet datos = new DataSet();
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(detallesConexion))
+                {
+                    string query = "SELECT Nombre, Departamento, Salario FROM Empleados";
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
+                    adaptador.Fill(datos, "Libros");
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error al obtener libros: " + ex.Message);
+            }
+
+            return datos;
+
+        }
+
+        public DataSet UpdateDatos()
+        {
+            DataSet datos = new DataSet();
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(detallesConexion))
+                {
+                    string query = "SELECT Nombre, Departamento, Salario FROM Empleados";
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
+                    adaptador.Fill(datos, "Libros");
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error al obtener libros: " + ex.Message);
+            }
+
+            return datos;
+
+        }
+
+        public DataSet DeleteDatos()
+        {
+            DataSet datos = new DataSet();
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(detallesConexion))
+                {
+                    string query = "SELECT Nombre, Departamento, Salario FROM Empleados";
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
+                    adaptador.Fill(datos, "Libros");
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error al obtener libros: " + ex.Message);
+            }
+
+            return datos;
+
+        }
+
     }
+
+
+
 }
